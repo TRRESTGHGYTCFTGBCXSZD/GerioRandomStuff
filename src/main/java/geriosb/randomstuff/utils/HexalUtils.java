b950976220718ca647733a766792ed6f546d290d
TRRESTGHGYTCFTGBCXSZD <40109698+TRRESTGHGYTCFTGBCXSZD@users.noreply.github.com>
2025-11-18 22:53:26 +0900

    ConcurrentModificationException begone

--- src/main/java/geriosb/randomstuff/utils/HexalUtils.java
+++ src/main/java/geriosb/randomstuff/utils/HexalUtils.java
@@ -3,12 +3,15 @@ package geriosb.randomstuff.utils;
 import ram.talia.hexal.api.config.HexalConfig;
 import ram.talia.hexal.api.mediafieditems.ItemRecord;
 
+import java.util.Iterator;
 import java.util.Map;
 
 public class HexalUtils {
     public static void FixMoteInvalidity(Map<Integer, ItemRecord> MoteInventory){
         // this will fix the invalid null slots with valid slots
-        for (Map.Entry<Integer, ItemRecord> elem : MoteInventory.entrySet()){
+        Iterator<Map.Entry<Integer, ItemRecord>> wan = MoteInventory.entrySet().iterator();
+        while (wan.hasNext()) {
+            Map.Entry<Integer, ItemRecord> elem = wan.next();
             int g = elem.getKey();
             ItemRecord ba = elem.getValue();
             MoteInventory.remove(elem.getKey());
